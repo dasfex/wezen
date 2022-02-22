b0496b98eb5b57a90066adbb8171dc8a76296467
dasfex <ixodor@mail.ru>
2022-02-22 16:56:18 +0300

    Fix metalist.

--- docs/metalist.md
+++ docs/metalist.md
@@ -6,32 +6,38 @@
 
 Signature:
 ```cpp
-template <class T, T... List> metalist_type -> 
+template <typename T, T... List> metalist -> 
     typename type = T
-    size_t size = size(List)
-    T head = head(List)
-    tail = metalist_type<T, tail(List)>;
-template <class T> metalist_type ->
-    typename type = T
-    size_t size = 0;
-template <int... List> metalist : metalist_type<int, List...>;
+    size_t size = size(List);
 ```
 
 Example:
 ```cpp
-wezen::metalist_type<int, 1, 2, 3>::type; // int
-wezen::metalist_type<int, 1, 2, 3>::size; // 3
-wezen::metalist_type<int, 1>::head; // 1
-typename wezen::metalist_type<int, 1>::template tail<>; // metalist_type<int>
-wezen::metalist_type<int>::size; // 0
-wezen::metalist<1, 2, -1, 4>::head; // 1
-typename wezen::metalist<1, 2, -1, 4>::template tail<>; // metalist<2, -1, 4>
+wezen::metalist<int, 1, 2, 3>::type; // int
+wezen::metalist<int, 1, 2, 3>::size; // 3
+wezen::metalist<int>::size; // 0
 ```
 
-There is some disadvantage of how ```tail``` is invoked but 
-this is because we want to decrease compilation time and 
-instantiate tail only if you use it.
-You can make ```using``` for better experience.
++ ```get_head, get_tail```
+Signature:
+```cpp
+template <
+        typename T, 
+        template <typename S, S...> typename Metalist,
+        T Head, T... Tail> -> get_head ->
+    T value = Head;
+template <
+        typename T, 
+        template <typename S, S...> typename Metalist,
+        T Head, T... Tail> -> get_tail ->
+    typename type = Metalist<T, Tail...>;
+```
+Examples:
+```cpp
+wezen::get_head<metalist<int, 0, 1, 2>>::value; // 0
+wezen::get_tail_t<metalist<int, 1, 2, 3, 4>>; // metalist<int, 2, 3, 4>
+```
+Look that ```get_tail_t``` exists but ```get_head_v``` not. 
 
 + ```get``` - return ```metalist[Ind]```.
 
@@ -43,25 +49,24 @@ template <0, Metalist> get -> Metalist::head;
 
 Example:
 ```cpp
-wezen::get<0, wezen::metalist<0, 1, 2>>::value; // 0
-wezen::get<2, wezen::metalist<0, 1, 2>>::value; // 2
-wezen::get_v<2, wezen::metalist_type<size_t, 0, 2, 7>>; // 7
+wezen::get<0, wezen::metalist<int, 0, 1, 2>>::value; // 0
+wezen::get<2, wezen::metalist<int, 0, 1, 2>>::value; // 2
+wezen::get_v<2, wezen::metalist<size_t, 0, 2, 7>>; // 7
 ```
 
 + ```contains``` - return is ```metalist``` contains ```Finded```.
 
 Signature:
 ```cpp
-template <class T, T Finded, Metalist> contains_type -> bool;
-template <int Finded, Metalist> contains -> bool;
+template <class T, T Finded, Metalist> contains -> bool;
 ```
 
 Example:
 ```cpp
-wezen::contains_type_v<int, 3, wezen::metalist<3, 2, 1>>; // true
-wezen::contains<2, wezen::metalist<3, 2, 1>>::value; // true
-wezen::contains_v<1, wezen::metalist<3, 2, 1>>; // true
-wezen::contains_v<0, wezen::metalist<3, 2, 1>>; // false
+wezen::contains_v<int, 3, wezen::metalist<int, 3, 2, 1>>; // true
+wezen::contains<int, 2, wezen::metalist<int, 3, 2, 1>>::value; // true
+wezen::contains_v<int, 1, wezen::metalist<int, 3, 2, 1>>; // true
+wezen::contains_v<int, 0, wezen::metalist<int, 3, 2, 1>>; // false
 ```
 
 + ```find``` - return index of ```Finded``` in ```metalist``` 
@@ -69,16 +74,15 @@ if it is in list or ```metalist::size``` if not.
 
 Signature:
 ```cpp
-template <class T, T Finded, Metalist> find_type -> size_t;
-template <int Finded, Metalist> find -> size_t;
+template <class T, T Finded, Metalist> find -> size_t;
 ```
 
 Example:
 ```cpp
-wezen::find_type_v<int, 2, wezen::metalist<1, 2, 3>>; // 1
-wezen::find<1, wezen::metalist<1, 2, 3>>::value; // 0
-wezen::find_v<3, wezen::metalist<1, 2, 3>>; // 2
-wezen::find_v<4, wezen::metalist<1, 2, 3>>; // 3
+wezen::find_v<int, 2, wezen::metalist<int, 1, 2, 3>>; // 1
+wezen::find<int, 1, wezen::metalist<int, 1, 2, 3>>::value; // 0
+wezen::find_v<int, 3, wezen::metalist<int, 1, 2, 3>>; // 2
+wezen::find_v<int, 4, wezen::metalist<int, 1, 2, 3>>; // 3
 ```
 
 + ```find_if``` - return position of the first element in ```metalist``` which satisfy to ```Predicate``` 
@@ -86,7 +90,10 @@ or size of there is no such elements.
 
 Signatures:
 ```cpp
-template <Metalist, template <typename Metalist::type> class Predicate> find_if -> size_t;
+template <
+        Metalist, 
+        template <typename Metalist::type> class Predicate
+        > find_if -> size_t;
 ```
 I.e. type of metalist elements and predicate should be the same.
 Examples:
@@ -99,6 +106,6 @@ template <int x>
 struct only_false {
     static constexpr bool value = false;
 };
-find_if<metalist<1, 3, 5, 6, 7>, is_even>::value; // 3
-find_if_v<metalist<1, 3, 5, 6, 7>, only_false>; // 5
+find_if<metalist<int, 1, 3, 5, 6, 7>, is_even>::value; // 3
+find_if_v<metalist<int, 1, 3, 5, 6, 7>, only_false>; // 5
 ```
--- includes/metalist/metalist_basics.hpp
+++ includes/metalist/metalist_basics.hpp
@@ -7,90 +7,56 @@
 
 namespace wezen {
 
-template<class T, T... List>
-struct metalist_type;
+// fail
+template <typename T = void, typename S = void>
+struct get_head;
 
-template<int... List>
-struct metalist;
-
-namespace details {
-template<class T, T Head, T... Tail>
-struct decompose_type {
-    static constexpr T head = Head;
-
-    template<class = void>
-    using tail = metalist_type<T, Tail...>;
+template <typename T, template <typename S, S...> typename Metalist, T Head, T... Tail>
+struct get_head<Metalist<T, Head, Tail...>> {
+    static constexpr T value = Head;
 };
 
-template<int Head, int... Tail>
-struct decompose {
-    static constexpr int head = Head;
+// fail
+template <typename T = void, typename S = void>
+struct get_tail;
 
-    template<class = void>
-    using tail = metalist<Tail...>;
-};
-} // namespace details
-
-template <class T, T... List>
-struct metalist_type {
-    using type = T;
-    using common = metalist_type;
-    static constexpr size_t size = sizeof...(List);
-    static constexpr T head = details::decompose_type<T, List...>::head;
-
-    template <class = void>
-    using tail = typename details::decompose_type<T, List...>::template tail<>;
+template <typename T, template <typename S, S...> typename Metalist, T Head, T... Tail>
+struct get_tail<Metalist<T, Head, Tail...>> {
+    using type = Metalist<T, Tail...>;
 };
 
-template <class T>
-struct metalist_type<T> {
-    using type = T;
-    using common = metalist_type;
-    static constexpr size_t size = 0;
-};
+template <typename Metalist>
+using get_tail_t = typename get_tail<Metalist>::type;
 
-template <int... List>
+template <typename T, T... List>
 struct metalist {
-    using type = int;
-    using common = metalist;
+    using type = T;
     static constexpr size_t size = sizeof...(List);
-    static constexpr int head = details::decompose<List...>::head;
-
-    template <class = void>
-    using tail = typename details::decompose<List...>::template tail<>;
 };
 
-template <>
-struct metalist<> {
-    using type = int;
-    using common = metalist;
-    static constexpr size_t size = 0;
-};
-
-template <size_t Ind, class Metalist>
+template <size_t Ind, typename Metalist>
 struct get {
     static_assert(Ind < Metalist::size);
-    static constexpr auto value = get<Ind - 1, typename Metalist::template tail<>>::value;
+    static constexpr auto value = get<Ind - 1, get_tail_t<Metalist>>::value;
 };
 
 template <class Metalist>
 struct get<0, Metalist> {
-    static_assert(Metalist::size >= 1);
-    static constexpr typename Metalist::type value = Metalist::head;
+    static_assert(Metalist::size > 0);
+    static constexpr typename Metalist::type value = get_head<Metalist>::value;
 };
 
-template <size_t Ind, class Metalist>
+template <size_t Ind, typename Metalist>
 constexpr inline auto get_v = get<Ind, Metalist>::value;
 
 template <class T, T Finded, class Metalist>
-struct contains_type {
-    static_assert(std::is_same_v<T, typename Metalist::type>);
+struct contains {
     static constexpr bool value = [] {
         if constexpr (Metalist::size > 0) {
-            if constexpr (Finded == Metalist::head) {
+            if constexpr (Finded == get_head<Metalist>::value) {
                 return true;
             } else {
-                return contains_type<T, Finded, typename Metalist::template tail<>>::value;
+                return contains<T, Finded, get_tail_t<Metalist>>::value;
             }
         } else {
             return false;
@@ -99,27 +65,19 @@ struct contains_type {
 };
 
 template <class T, T Finded, class Metalist>
-constexpr inline bool contains_type_v = contains_type<T, Finded, Metalist>::value;
-
-template <int Finded, class Metalist>
-struct contains {
-    static constexpr bool value = contains_type_v<int, Finded, Metalist>;
-};
-
-template <int Finded, class Metalist>
-constexpr inline bool contains_v = contains<Finded, Metalist>::value;
+constexpr inline bool contains_v = contains<T, Finded, Metalist>::value;
 
 namespace details {
 
 template <class T, T Finded, class Metalist, size_t Size = Metalist::size>
-struct find_type_impl {
+struct find_impl {
     static_assert(std::is_same_v<T, typename Metalist::type>);
     static constexpr size_t value = [] {
         if constexpr (Metalist::size > 0) {
-            if constexpr (Finded == Metalist::head) {
+            if constexpr (Finded == get_head<Metalist>::value) {
                 return 0;
             } else {
-                constexpr auto result = find_type_impl<T, Finded, typename Metalist::template tail<>, Size>::value;
+                constexpr auto result = find_impl<T, Finded, get_tail_t<Metalist>, Size>::value;
                 if constexpr (result == Size) {
                     return result;
                 } else {
@@ -136,10 +94,10 @@ template <class Metalist, template <typename Metalist::type> class Predicate,  s
 struct find_if_impl {
     static constexpr size_t value = [] {
         if constexpr (Metalist::size > 0) {
-            if constexpr (Predicate<Metalist::head>::value) {
+            if constexpr (Predicate<get_head<Metalist>::value>::value) {
                 return 0;
             } else {
-                constexpr auto result = find_if_impl<typename Metalist::template tail<>, Predicate, Size>::value;
+                constexpr auto result = find_if_impl<get_tail_t<Metalist>, Predicate, Size>::value;
                 if constexpr (result == Size) {
                     return result;
                 } else {
@@ -155,20 +113,12 @@ struct find_if_impl {
 } // namespace details
 
 template <class T, T Finded, class Metalist>
-struct find_type {
-    static constexpr auto value = details::find_type_impl<T, Finded, Metalist>::value;
+struct find {
+    static constexpr auto value = details::find_impl<T, Finded, Metalist>::value;
 };
 
 template <class T, T Finded, class Metalist>
-constexpr inline auto find_type_v = find_type<T, Finded, Metalist>::value;
-
-template <int Finded, class Metalist>
-struct find {
-    static constexpr auto value = find_type<int, Finded, Metalist>::value;
-};
-
-template <int Finded, class Metalist>
-constexpr inline auto find_v = find<Finded, Metalist>::value;
+constexpr inline auto find_v = find<T, Finded, Metalist>::value;
 
 template <class Metalist, template <typename Metalist::type> class Predicate>
 struct find_if {
@@ -179,36 +129,32 @@ template <class Metalist, template <typename Metalist::type> class Predicate>
 constexpr inline auto find_if_v = find_if<Metalist, Predicate>::value;
 
 #ifdef TEST
-/// decompose_type
-static_assert(details::decompose_type<int, 1, 2, 3, 4>::head == 1);
-static_assert(std::is_same_v<typename details::decompose_type<int, 1, 2, 3, 4>::template tail<>,
-        metalist_type<int, 2, 3, 4>>);
 
 /// metalist
-static_assert(std::is_same_v<metalist_type<int, 1, 2, 3>::type, int>);
-static_assert(metalist_type<int, 1, 2, 3>::size == 3);
-static_assert(metalist_type<int, 1>::head == 1);
-static_assert(std::is_same_v<typename metalist_type<int, 1>::template tail<>, metalist_type<int>>);
-static_assert(std::is_same_v<typename metalist_type<int, 1, 2>::template tail<>, metalist_type<int, 2>>);
-static_assert(metalist_type<int>::size == 0);
-static_assert(metalist<1, 2, -1, 4>::head == 1);
+static_assert(std::is_same_v<metalist<int, 1, 2, 3>::type, int>);
+static_assert(metalist<int, 1, 2, 3>::size == 3);
+static_assert(get_head<metalist<int, 0, 1, 2, 3>>::value == 0);
+static_assert(std::is_same_v<get_tail<metalist<int, 1>>::type, metalist<int>>);
+static_assert(std::is_same_v<get_tail_t<metalist<int, 1, 2, 3, 4>>, metalist<int, 2, 3, 4>>);
+static_assert(metalist<int>::size == 0);
+static_assert(get_head<metalist<int, 2, -1, 4>>::value == 2);
 
 /// get
-static_assert(get<0, metalist<0, 1, 2>>::value == 0);
-static_assert(get<2, metalist<0, 1, 2>>::value == 2);
-static_assert(get_v<2, metalist_type<size_t, 0, 2, 7>> == 7);
+static_assert(get<0, metalist<int, 0, 1, 2>>::value == 0);
+static_assert(get<2, metalist<int, 0, 1, 2>>::value == 2);
+static_assert(get_v<2, metalist<size_t, 0, 2, 7>> == 7);
 
 /// contains
-static_assert(contains_type_v<int, 3, metalist<3, 2, 1>>);
-static_assert(contains<2, metalist<3, 2, 1>>::value);
-static_assert(contains_v<1, metalist<3, 2, 1>>);
-static_assert(!contains_v<0, metalist<3, 2, 1>>);
+static_assert(contains_v<int, 3, metalist<int, 3, 2, 1>>);
+static_assert(contains<int, 2, metalist<int, 3, 2, 1>>::value);
+static_assert(contains_v<int, 1, metalist<int, 3, 2, 1>>);
+static_assert(!contains_v<int, 0, metalist<int, 3, 2, 1>>);
 
 /// find
-static_assert(find_type_v<int, 2, metalist<1, 2, 3>> == 1);
-static_assert(find<1, metalist<1, 2, 3>>::value == 0);
-static_assert(find_v<3, metalist<1, 2, 3>> == 2);
-static_assert(find_v<4, metalist<1, 2, 3>> == 3);
+static_assert(find_v<int, 2, metalist<int, 1, 2, 3>> == 1);
+static_assert(find<int, 1, metalist<int, 1, 2, 3>>::value == 0);
+static_assert(find_v<int, 3, metalist<int, 1, 2, 3>> == 2);
+static_assert(find_v<int, 4, metalist<int, 1, 2, 3>> == 3);
 
 /// find_if
 template <int x>
@@ -219,8 +165,8 @@ template <int x>
 struct only_false {
     static constexpr bool value = false;
 };
-static_assert(find_if<metalist<1, 3, 5, 6, 7>, is_even>::value == 3);
-static_assert(find_if_v<metalist<1, 3, 5, 6, 7>, only_false> == 5);
+static_assert(find_if<metalist<int, 1, 3, 5, 6, 7>, is_even>::value == 3);
+static_assert(find_if_v<metalist<int, 1, 3, 5, 6, 7>, only_false> == 5);
 #endif
 
 } // namespace wezen
